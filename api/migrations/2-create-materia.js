3826b67be710a06378542b5d927a5f49c4add495
mauriciopintos <mauricioenrique.pintos@estudiantes.unahur.edu.ar>
2022-09-11 17:58:33 -0300

    Se actualizan los archivos migrations/2-create-materia.js y modelo/materia.js

--- api/migrations/2-create-materia.js
+++ api/migrations/2-create-materia.js
@@ -11,6 +11,9 @@ module.exports = {
       nombre: {
         type: Sequelize.STRING
       },
+      id_carrera: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- api/models/materia.js
+++ api/models/materia.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const materia = sequelize.define('materia', {
-    nombre: DataTypes.STRING
+    nombre: DataTypes.STRING,
+    id_carrera: DataTypes.INTEGER
   }, {});
   
   return materia;
