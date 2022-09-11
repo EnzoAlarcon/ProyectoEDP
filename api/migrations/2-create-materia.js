b9cc2f3ebe0fd1336febd01a4b73d6752b886d59
mauriciopintos <mauricioenrique.pintos@estudiantes.unahur.edu.ar>
2022-09-11 17:06:46 -0300

    Se crea el archivo migrations/2-create-materia.js para crear la tabla en el modelo

---
+++ api/migrations/2-create-materia.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('materias', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      apellido: {
+        type: Sequelize.STRING
+      },
+      dni: {
+        type: Sequelize.INTEGER
+      },
+      nacimiento: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('materias');
+  }
+};
